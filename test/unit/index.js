7c42846a6ae4d83901014240c17e890ca4b67ab5
facugait <facugait@users.noreply.github.com>
2019-10-23 21:38:53 -0300

    tests added

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,8 +1,42 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
+const ctrl = new (require('../../src/application/controllers/user.controller'));
 
-describe('Dummy test', () => {
-  it('Should just be here as to make tests pass', (done) => {
-    assert(true);
-    done();
+describe('Get Users', () => {
+  it('Should return and array of objcts with users', async () => {
+    const mockReq = {};
+
+    const mockRes = {
+      status(status) {
+        return {
+          json(obj) {
+            return obj;
+          }
+        };
+      }
+    };
+
+    const result = await ctrl.getUsers(mockReq, mockRes);
+
+    expect(result.user).to.be.an('array');
+  });
+});
+
+describe('Get Active Users', () => {
+  it('Should return and array of objcts with active users', async () => {
+    const mockReq = {};
+
+    const mockRes = {
+      status(status) {
+        return {
+          json(obj) {
+            return obj;
+          }
+        };
+      }
+    };
+
+    const result = await ctrl.getActiveUsers(mockReq, mockRes);
+
+    expect(result.user).to.be.an('array');
   });
 });
